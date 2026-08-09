fbb6d525d2e8957038590965568a20fadabd5995
kaioumdev <afrankaiyum34@gmail.com>
2026-08-10 00:53:52 +0600

    successfully fix the erros

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      '*.glsl': {
+        loaders: ['raw-loader'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
